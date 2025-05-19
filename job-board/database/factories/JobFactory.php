3f1392ac4690eca99b3f8068c6c9266a31d7466e
Patanjalipandey <76595705+Patanjalipandey@users.noreply.github.com>
2025-05-20 00:12:11 +0530

    Job Model created and dtat seeded succesfully

---
+++ job-board/database/factories/JobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle,
+            'description' => fake()->paragraphs(3, true),
+            'salary' => fake()->numberBetween(5000, 150000),
+            'location' => fake()->city,
+            'category' => fake()->randomElement(['IT', 'Finance', 'Marketing', 'Sales']),
+            'experience' => fake()->randomElement(['entry', 'intermediate', 'senior'])
+        ];
+    }
+}
